18995dee1a7eaed06376d85f4733cbb5799fddac
Gul AM <gul.amd.git@gmail.com>
2023-01-12 20:06:24 +0200

    chore: still working on the authentication

--- src/composables/UseAuthUser.js
+++ src/composables/UseAuthUser.js
@@ -1,19 +1,55 @@
 import { ref } from 'vue'
+import { useSupabase } from 'boot/supabase'
 
 const user = ref(null)
 export default function useAuthUser () {
-  const login = async ({ email, password }) => {}
-  const loginWithSocialProvider = async (provider) => {}
-  const logout = async () => {}
+  const { supabase } = useSupabase()
+
+  const login = async ({ email, password }) => {
+    const { user, error } = await supabase.auth.SignIn({ email, password })
+    if (error) throw error
+    return user
+  }
+
+  const loginWithSocialProvider = async (provider) => {
+    const { user, error } = await supabase.auth.SignIn({ provider })
+    if (error) throw error
+    return user
+  }
+
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) throw error
+    return error
+  }
+
   const isLoggedIn = () => {
     return !!user.value
   }
 
-  const register = async ({ email, password, ...meta }) => {}
+  const register = async ({ email, password, ...meta }) => {
+    const { user, error } = await supabase.auth.signUp(
+      { email, password },
+      {
+        data: meta,
+        redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`
+      }
+    )
+    if (error) throw error
+    return user
+  }
 
-  const update = async (data) => {}
+  const update = async (data) => {
+    const { user, error } = await supabase.auth.update(data)
+    if (error) throw error
+    return user
+  }
 
-  const sendPasswordResetEmail = async () => {}
+  const sendPasswordResetEmail = async () => {
+    const { user, error } = await supabase.auth.api.resetPasswordForEmail(email)
+    if (error) throw error
+    return user
+  }
 
   return {
     login,
