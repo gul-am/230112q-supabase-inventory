730516b9125cf1b2953e6e3b08057f877385496a
Gul AM <gul.amd.git@gmail.com>
2023-01-13 11:22:59 +0200

    chore:  creating the login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page padding>
+    <p class="text-h6">
+      Login
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageLogin'
+})
+</script>
