3bd4fabc3bea2f2a7d7c5c3c9c7bdff605b00e7e
Gul AM <gul.amd.git@gmail.com>
2023-01-13 23:09:41 +0200

    chore: still working on the login layout

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-   <q-form class="row justify-center" @submit.prevent="">
+   <q-form class="row justify-center" @submit.prevent="handleLogin">
     <p class="col-12 text-h4 text-center"> Login </p>
     <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
       <q-input
@@ -21,6 +21,24 @@
         type="submit"
       />
     </div>
+    <div class="full-width q-pt-sm">
+      <q-btn
+        label="Register"
+        color="primary"
+        class="full-width"
+        flat
+        to="/register"
+      />
+    </div>
+    <div class="full-width q-pt-md">
+      <q-btn
+        label="Contact Us"
+        color="primary"
+        class="full-width"
+        flat
+        to="/contacts"
+      />
+    </div>
 
     </div>
    </q-form>
@@ -29,18 +47,34 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import useAuthUser from 'src/composables/UseAuthUser'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'PageLogin',
 
   setup () {
+    const router = useRouter()
+
+    const { login } = useAuthUser()
+
     const form = ref({
       email: '',
       password: ''
     })
 
+    const handleLogin = async () => {
+      try {
+        await login(form.value)
+        router.push({ name: 'me' })
+      } catch (error) {
+        alert(error.message)
+      }
+    }
+
     return {
-      form
+      form,
+      handleLogin
     }
   }
 
