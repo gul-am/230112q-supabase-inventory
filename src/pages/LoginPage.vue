13238dcd4a30ca13d9bd85f455558c39eb634ab7
Gul AM <gul.amd.git@gmail.com>
2023-01-13 12:01:31 +0200

    build: Login page layout

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,15 +1,48 @@
 <template>
   <q-page padding>
-    <p class="text-h6">
-      Login
-    </p>
+   <q-form class="row justify-center" @submit.prevent="">
+    <p class="col-12 text-h4 text-center"> Login </p>
+    <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
+      <q-input
+        label="Email"
+        v-model="form.email"
+      />
+      <q-input
+        label="password"
+        v-model="form.password"
+      />
+
+    <div class="full-width q-pt-md">
+      <q-btn
+        label="Login"
+        color="primary"
+        class="full-width"
+        outline
+        type="submit"
+      />
+    </div>
+
+    </div>
+   </q-form>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'PageLogin'
+  name: 'PageLogin',
+
+  setup () {
+    const form = ref({
+      email: '',
+      password: ''
+    })
+
+    return {
+      form
+    }
+  }
+
 })
 </script>
