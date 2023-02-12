ff652798b5ee0e79e2fc7452fe207f7784f9e523
gul-amd <gul.amd.git@gmail.com>
2023-02-12 12:26:59 +0200

    build: email confirmation page

---
+++ src/pages/EmailConfirmationPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding >
+    <p class="text-body1">
+      Thanks for registering
+    </p>
+    <p class="text-body1">
+      please confirm your email to finilize your registration: <strong>{{ $route.query.email }}</strong>
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation'
+})
+</script>
