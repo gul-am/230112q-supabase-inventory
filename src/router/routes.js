7b2401cba904d1b36456d5eace39b1b0e8558f24
gul-amd <gul.amd.git@gmail.com>
2023-02-12 12:25:01 +0200

    chore: amending routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,11 @@ const routes = [
     component: () => import('layouts/LoginLayout.vue'),
     children: [
       { path: '', name: 'loginDefault', component: () => import('pages/LoginPage.vue') },
-      { path: 'login', name: 'login', component: () => import('pages/LoginPage.vue') }
+      { path: 'login', name: 'login', component: () => import('pages/LoginPage.vue') },
+      { path: 'register', name: 'register', component: () => import('pages/RegisterPage.vue') },
+      { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmationPage.vue') },
+      { path: 'me', name: 'me', component: () => import('pages/MePage.vue') }
+
     ]
   },
   {
